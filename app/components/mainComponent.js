3e0e5942650e1c7e06af9e6bed8e79330b848124
onedoar <andreyz@one1.co.il>
2016-09-04 07:36:35 +0300

    init

---
+++ app/components/mainComponent.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+app.component("main", {
+    templateUrl: "app/templates/main.html",
+    controller: function (mainService) {
+        this.board = mainService.board;
+        this.players = mainService.players;
+
+        this.addState = function (state, index) {
+            if (state != 0) {
+                alert("not posible");
+                return
+            }
+
+            this.board.boardStates[index] = 1;
+
+            if (!this.checkIfWin(this.players["id2"].states)) {
+                this.changePlayerSide();
+            } else {
+                setTimeout(() => { alert("you win") }, 100)
+            }
+        }
+
+        this.checkIfWin = function (states) {
+            return this.board.winState.includes(parseInt(states.join(""), 2));
+        }
+
+        this.changePlayerSide = function () {
+
+        }
+    }
+})
---
+++ script.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module("krestiki", ["ui.router"]);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/main");
+
+    $stateProvider
+    .state("main", {
+        url: "/main",
+        template: "<main></main>"
+    })
+})
